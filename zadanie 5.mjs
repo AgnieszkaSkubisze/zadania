92e199655f7decdc7ef2283ef0803354303fca60
Agnieszka Skubiszewska <askubiszewska@g2a.com>
2020-07-13 15:51:12 +0200

    Zadanie 5 - wprowadzenie switch

---
+++ zadanie 5.mjs
@@ -0,0 +1,26 @@
+
+//uzywajac for i switch stworz metodę, ktora przyjmuje jeden parametr jako tablica i zwraca 
+//tablice skladajaca sie z imion : [adam,justyna,iwona]
+
+names = ['adam','ala','agnieszka','justyna','magda','dzesika','iwona']
+
+pickedNames =[]
+
+function nameChoosing(name) {
+    for (let i = 0; i < name.length; i++) {
+        let nextName = name[i]
+        switch(nextName) {
+            case 'adam':
+                pickedNames.push(nextName)
+            break
+            case 'justyna':
+                pickedNames.push(nextName)
+            break
+            case 'iwona':
+                pickedNames.push(nextName)
+            break
+        }
+    }
+    return pickedNames
+}
+console.log(nameChoosing(names))
