af8fbd025d0e0942a8fde350c1e9787e2706f307
Agnieszka Skubiszewska <askubiszewska@g2a.com>
2020-07-21 12:26:04 +0200

    Zadanie 8,9

---
+++ zadanie 8.mjs
@@ -0,0 +1,42 @@
+
+//Masz zrobić trzy metody 
+// metoda 1 - przujume liczbe, jezeli parametr nie jest liczba zwraca 0, jesli jest liczba to zwieksza o jeden, i zwraca liczbe
+// metoda 2  - przyjmuje libcze jezeli nie jest liczba zwraca 0, jesli jest zmniejsza o jeden i zwraca liczbe 
+// metoda 3 - metoda ktora sprawdza dwie tablice, i jezeli jakas liczba wystepuje w obu tablicach i jest parzysta to metoda 1
+//jezeli wystepuje i jest nieparzysta to metoda 2, ta metoda zwraca wynik jako tablica 
+tab1 = [2,3,4,5,6,7,8,9]
+tab2 = [2,4,6,7,9]
+
+
+function thisIsAddedNumber(someNumbers){
+    if(typeof someNumbers !== 'number'){ 
+        return 0
+    } else {
+        return someNumbers + 1
+    } 
+}
+
+function thisIsSubstractOne(someNumbers){
+
+    if(typeof someNumbers !== 'number'){ 
+        return 0
+    } else {
+        return someNumbers - 1
+    }
+}
+
+function checkThoseTabs(){
+    tab3 = []
+    for (let i = 0; i < tab1.length; i++) {
+        for (let j = 0; j < tab2.length; j++) {
+            let theyAreTheSame = tab1[i] === tab2[j]
+            if (theyAreTheSame && (tab1[i] % 2 ==0)){
+                 tab3.push(thisIsAddedNumber(tab1[i]))
+            }
+                else if (theyAreTheSame && (tab1[i] % 2 !==0))
+                    tab3.push(thisIsSubstractOne(tab1[i]))
+                }
+            }
+            return tab3
+        }
+        console.log(checkThoseTabs())
---
+++ zadanie 9.mjs
@@ -0,0 +1,48 @@
+////Masz zrobić trzy metody 
+// metoda 1 - przyjmuje obiekt, zwieksza wlasnisc value o 1, zwraca edytowany obiekt 
+// metoda 2  - przyjmuje obiekt , zmniejsza wlasnisc value o 1, zwraca edytowany obiekt 
+// metoda 3 - przyjmuje tablice obiektów, i zwraca tablice obiektów, dla parzystych id z obiektu tablicy wykonywana jest metoda 1 
+//a dla nieparzystych  jest metoda 2, wynikiem dzialania metody jest nowa tablica obiektów 
+tab1 = [{id:2,value:5},{id:3,value:5},{id:4,value:5},{id:5,value:5},{id:6,value:5}]
+
+
+function addOne(exampleObject){
+
+    if( typeof exampleObject  === 'object'){ 
+        exampleObject.value = exampleObject.value +1
+    } 
+    return exampleObject 
+  }
+  
+  function substractOne(secondExampleObject){
+  
+      if(typeof secondExampleObject === 'object'){ 
+        secondExampleObject.value = secondExampleObject.value - 1
+         }
+    return  secondExampleObject
+  }
+  
+ 
+function letMixMyMethods(tab1){
+
+    tab3 =  []
+    
+    for (let i = 0; i < tab1.length; i++) {
+       
+          const even = tab1[i].id % 2 == 0
+            if(even){
+                tab3.push(addOne(tab1[i]))
+            } else {
+                tab3.push(substractOne(tab1[i]))
+            }
+        
+    }
+    return tab3
+}
+
+  console.log(letMixMyMethods(tab1))
+
+
+
+
+
