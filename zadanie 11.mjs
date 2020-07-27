947dbdb74829ed01fcd5ae978a8ef22279c23bc6
Agnieszka Skubiszewska <askubiszewska@g2a.com>
2020-07-27 13:32:23 +0200

    Zadanie 11

---
+++ zadanie 11.mjs
@@ -0,0 +1,9 @@
+////Masz zrobić trzy metody 
+// metoda 1 - przyjmuje dwa parametry, zwraca sumę 
+// metoda 2  - przyjmuje dwa parametry, zwraca róźnice 
+// metoda 3 - przyjmuje 2 tablice obiektów, i zwraca tablice obiektów, jesli id w obu tablicach jest takie
+// samo, to dla  dla parzystych id z obiektu tablicy wykonywana jest metoda 1 i zwraca sume value
+//a dla nieparzystych  jest metoda 2 ktora zwraca róźniec value , wynikiem dzialania metody jest nowa tablica obiektów 
+tab1 = [{id:2,value:5},{id:3,value:5},{id:4,value:5},{id:5,value:5},{id:6,value:5}]
+tab2 = [{id:2,value:5},{id:3,value:5}]
+
